cabc630ab1dc0d692a0d16c304067578d7f2d69c
Micah Stalberg <stalberm@oregonstate.edu>
2022-06-07 11:46:10 -0700

    Removal middle code from story, now use middle function in story

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -3,12 +3,6 @@
 
 def story
   first = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
-  middle = ""
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
   conclusion = "And that was it."
   first + middle + conclusion
 end
